54c9ab6bbcb1fc28868de19885a8a2e3a88f3d24
kheraud <kheraud@fastmail.com>
2020-12-04 18:45:37 +0100

    feat(ui): add filter mecanism for repository status

--- ui/src/components/RepoList.vue
+++ ui/src/components/RepoList.vue
@@ -6,8 +6,23 @@
       :length="pageCount"
       total-visible="7"
     ></v-pagination>
-    <v-select v-model="sort" :items="sortList" />
-    <v-select v-model="statuses" :items="statusList" />
+    <v-container fluid>
+      <v-row align="center">
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select v-model="sort" :items="sortList" label="Sort" />
+        </v-col>
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            v-model="statuses"
+            :items="statusList"
+            label="Status"
+            multiple
+            clearable
+            placeholder="All"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
     <div v-if="repos !== null" class="d-flex justify-space-around flex-wrap">
       <Repository v-for="repo in repos" :key="repo.name" :content="repo" />
     </div>
@@ -50,7 +65,7 @@ export default {
       sortList: SelectRepositorySort,
       statusList: SelectRepositoryStatus,
 
-      sort: SelectRepositorySort[0],
+      sort: SelectRepositorySort[0].value,
       statuses: null,
 
       repos: null,
@@ -70,11 +85,26 @@ export default {
     page: function() {
       this.fetchList();
     },
+    statuses: function() {
+      this.fetchList();
+    },
   },
   methods: {
     fetchList: function() {
       this.repos = null;
-      let query = [
+
+      let statusQuery = [];
+
+      if (this.statuses != null) {
+        statusQuery = this.statuses.map((x) => {
+          return {
+            key: "status",
+            value: x,
+          };
+        });
+      }
+
+      let queryParameters = [
         {
           key: "page",
           value: this.category,
@@ -91,7 +121,10 @@ export default {
           key: "offset",
           value: (this.page - 1) * RepositoryPerPage,
         },
-      ]
+        ...statusQuery,
+      ];
+
+      let query = queryParameters
         .filter((x) => x.value != null)
         .map((x) => x.key + "=" + x.value)
         .join("&");
