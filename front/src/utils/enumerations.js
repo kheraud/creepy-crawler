3539ec0a28ebad9ab534cee409c94926269f687d
kheraud <kheraud@fastmail.com>
2020-12-14 18:07:36 +0100

    feat(front): add a app.config.js in front app, closes #7 (#39)

--- front/src/utils/enumerations.js
+++ front/src/utils/enumerations.js
@@ -1,41 +1,10 @@
-const RepositoryStatus = [
-  {
-    id: 0,
-    label: "To analyze",
-    color: "blue-grey darken-3",
-    icon: "mdi-help-circle-outline",
-  },
-  {
-    id: 1,
-    label: "To test",
-    color: "orange accent-4",
-    icon: "mdi-flask-empty-outline",
-  },
-  {
-    id: 2,
-    label: "Dropped",
-    color: "blue-grey lighten-3",
-    icon: "mdi-minus-circle-outline",
-  },
-  {
-    id: 3,
-    label: "To implement",
-    color: "deep-orange accent-3",
-    icon: "mdi-shovel",
-  },
-  {
-    id: 4,
-    label: "Implemented",
-    color: "green darken-3",
-    icon: "mdi-thumb-up-outline",
-  }
-];
+import { status as repoStatus } from "../../app.config"
 
 const MapRepositoryStatus = new Map(
-  RepositoryStatus.map(key => [key.id, key])
+  repoStatus.map(key => [key.id, key])
 );
 
-const SelectRepositoryStatus = RepositoryStatus.map((x) => {
+const SelectRepositoryStatus = repoStatus.map((x) => {
   return {
     value: x.id,
     text: x.label,
@@ -50,12 +19,8 @@ const SelectRepositorySort = [
   { text: "Status", value: 'status' },
 ];
 
-const RepositoryPerPage = 40;
-
 export {
   SelectRepositorySort,
-  RepositoryStatus,
   MapRepositoryStatus,
   SelectRepositoryStatus,
-  RepositoryPerPage,
 };
